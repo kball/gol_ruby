21c9a73e7379498b6c52e2847a8a5faea8f92cd0
Kevin Ball <kmball11@gmail.com>
2015-10-19 09:28:06 -0700

    Beginnings of sparse matrix rep

---
+++ app/models/node.rb
@@ -0,0 +1,76 @@
+class Node
+  attr_accessor :left, :right, :parent, :value, :count, :subtree
+
+  def initialize(value, subvalue = nil, parent = nil)
+    self.value = value
+    self.parent = parent
+    self.left = nil
+    self.right = nil
+    self.count = 1
+    self.subtree = Node.new(subvalue) if subvalue
+  end
+
+  def add_node(value, subvalue = nil)
+    if value < self.value
+      self.left ? self.left.add_node(value, subvalue) :
+                  self.left = Node.new(value, subvalue, self)
+    elsif value > self.value
+      self.right ? self.right.add_node(value, subvalue) :
+                  self.right = Node.new(value, subvalue, self)
+    else
+      if subvalue
+        self.subtree.add_node(subvalue)
+      else
+        self.count += 1
+      end
+    end
+  end
+
+  def each(&block)
+    self.left.each(&block) if self.left
+    yield self
+    self.right.each(&block) if self.right
+  end
+
+  def to_a
+    a = []
+    self.each {|x| a.push(x)}
+    a
+  end
+
+  def prune(val_to_keep = 3)
+    self.left.prune(val_to_keep) if self.left
+    if self.subtree
+      self.subtree.prune(val_to_keep)
+    else
+      self.remove unless self.value == val_to_keep
+    end
+    self.right.prune(val_to_keep) if self.right
+  end
+
+  def replace_child(node, replacement)
+    if self.left == node
+      self.left = replacement
+      replacement.parent = self if replacement
+    elsif self.right == node
+      self.right = replacement
+      replacement.parent = self if replacement
+    end
+  end
+
+  def remove
+    if self.left == nil && self.right == nil
+      self.parent.replace_child(self, nil)
+    elsif self.left == nil
+      self.parent.replace_child(self, self.left)
+    elsif self.right == nil
+      self.parent.replace_child(self, self.right)
+    else
+      replacement = self.find_minimum(right)
+      self.value = replacement.value
+      self.count = replacement.count
+      self.subtree = replacement.subtree
+      replacement.remove
+    end
+  end
+end
---
+++ specs/models/node.rb
@@ -0,0 +1,17 @@
+require File.join(File.dirname(__FILE__), '../../app/models/node')
+
+describe Node do
+
+  it "Should create a set of nested binary trees for x/y coordinates" do
+    node = Node.new(1, 5)
+    node.add_node(1, 4)
+    node.add_node(1, 3)
+    node.add_node(1, 2)
+    node.add_node(2, 5)
+    node.left.should == nil
+    node.right.value.should == 2
+    node.to_a.size.should == 2
+    node.subtree.to_a.size.should == 4
+    node.subtree.to_a.map(&:value).should == [2,3,4,5]
+  end
+end
